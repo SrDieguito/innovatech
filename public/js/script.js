68fe7c4036e8622d9401d12d773d533bd91fc4a6
Sr_Dieguito <crafteo727@gmail.com>
2025-03-05 19:32:52 -0500

    arreglo db conexion

--- public/js/script.js
+++ public/js/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const data = Object.fromEntries(formData.entries());
 
         try {
-            const response = await fetch("/api/procesar_formulario", {
+            const response = await fetch("/api/procesarformulario", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data),
