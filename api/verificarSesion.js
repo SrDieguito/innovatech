8911bcae83d0001ae7b722d6f0b1b06b77beae27
Sr_Dieguito <crafteo727@gmail.com>
2025-03-13 23:24:40 -0500

    panel

---
+++ api/verificarSesion.js
@@ -0,0 +1,10 @@
+import { parse } from "cookie";
+
+export default function handler(req, res) {
+    const cookies = parse(req.headers.cookie || ""); 
+    if (cookies.token) {
+        res.status(200).json({ autenticado: true });
+    } else {
+        res.status(401).json({ autenticado: false });
+    }
+}
