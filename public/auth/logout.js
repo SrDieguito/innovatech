54d13725f5641837f57ad56e0978b034eae01a29
Sr_Dieguito <crafteo727@gmail.com>
2025-03-14 00:13:40 -0500

    panel

--- public/auth/logout.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader("Set-Cookie", "token=; HttpOnly; Path=/; Max-Age=0"); // Elimina la cookie
-    res.status(200).json({ message: "Sesión cerrada" });
-}
