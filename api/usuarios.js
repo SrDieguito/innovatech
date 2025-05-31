a1ad52ed482545fa650d36d48feeca5c04a24b41
Lucas Duvan <SrDieguito>
2025-05-30 19:26:07 -0500

    betesda

--- api/usuarios.js
+++ api/usuarios.js
@@ -1,6 +1,16 @@
 // File: /api/usuarios.js
 
-import { pool } from "@/lib/db";
+import mysql from "mysql2/promise";
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
