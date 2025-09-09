4043048c664145c2f95c3ce05e681f66b23a13c8
Sr_Dieguito <crafteo727@gmail.com>
2025-09-09 00:03:22 -0500

    estado

--- api/entregas.js
+++ api/entregas.js
@@ -1,6 +1,13 @@
 // api/entregas.js
 import multer from 'multer';
-import { pool } from '/api/db.js';
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectionLimit: 10,
+});
 
 /* ===== Helpers ===== */
 function getUserId(req) {
--- api/tareas.js
+++ api/tareas.js
@@ -1,6 +1,11 @@
-// api/tareas.js
-import { pool } from '/api/db.js';
-
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectionLimit: 10,
+});
 /* ===== Helpers ===== */
 function getUserId(req) {
   return req.cookies?.user_id || null; // ajusta si tu cookie se llama distinto
